afcc1213844d2a1fa56f4ff6abc81ba291521a05
Jeremiah C. Foster <jeremiah.foster@pelagicore.com>
2015-08-31 12:09:28 +0200

    Corrected name. Added SPDX header.

--- spdxl.pl
+++ spdxl.pl
@@ -5,7 +5,7 @@ use strict;
 
 =head1 NAME
 
-spdxl.pm  -- script that attempts to identify FOSS licenses contained with files and directories.
+spdxl.pl  -- script that attempts to identify FOSS licenses contained with files and directories.
 
 =head1 VERSION
 
@@ -18,6 +18,38 @@ our $VERSION = '0.1.0';
 =head1 SYNOPSIS
 
 This file is a script where many of the actions that spdxl executes are stored. 
+
+=head1 LICENSE
+
+GPLv3
+
+=over
+
+ SPDX license identifier: GPL-3.0
+
+ Copyright (C) 2015, Jeremiah C. Foster <jeremiah@jeremiahfoster.com>
+
+ This file is part of spdxl.
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ Boston, MA 02110-1301, USA.
+
+ List of changes:
+ Aug. 2015, spdxl.pl, created file
+
+=back
 
 =cut
 
