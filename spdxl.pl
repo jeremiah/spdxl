b27890364e1202089c7af1b3af715c2e99d1f096
Jeremiah C. Foster <jeremiah.foster@pelagicore.com>
2015-08-31 13:41:57 +0200

    Upgrading to GPLv3 properly.

--- spdxl.pl
+++ spdxl.pl
@@ -25,6 +25,8 @@ GPLv3
 
 =over
 
+ spdxl  -- license identifier with reporting
+
  SPDX license identifier: GPL-3.0
 
  Copyright (C) 2015, Jeremiah C. Foster <jeremiah@jeremiahfoster.com>
@@ -33,7 +35,7 @@ GPLv3
 
  This program is free software; you can redistribute it and/or
  modify it under the terms of the GNU General Public License
- as published by the Free Software Foundation; either version 2
+ as published by the Free Software Foundation; either version 3
  of the License, or (at your option) any later version.
 
  This program is distributed in the hope that it will be useful,
