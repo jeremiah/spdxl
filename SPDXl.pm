251a3d76af605f6a70769f90224771527340d1ab
Jeremiah C. Foster <jeremiah.foster@pelagicore.com>
2015-09-03 22:30:03 +0200

    Adding header to SPDXl.pm

--- SPDXl.pm
+++ SPDXl.pm
@@ -21,4 +21,40 @@ This file holds various subroutines that spdxl.pl reuses.
 
 =cut
 
+=head1 LICENSE
+
+GPLv3
+
+=over
+
+ spdxl  -- license identifier with reporting
+
+ SPDX license identifier: GPL-3.0
+
+ Copyright (C) 2015, Jeremiah C. Foster <jeremiah@jeremiahfoster.com>
+
+ This file is part of spdxl.
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 3
+ of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ Boston, MA 02110-1301, USA.
+
+ List of changes:
+ Aug. 2015, spdxl.pl, created file
+
+=back
+
+=cut
+
 
